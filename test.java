38e2388f055f74e981f11f87f639ee2c26d5ae28
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-11-12 10:18:19 -0700

    selenium test template (Java, Firefox)

---
+++ test.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Firefox.FirefoxDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args){
+
+        WebDriver driver = new FirefoxDriver()
+
+        driver.get(https://nicholasanjewierde4.wixsite.com/nickportfolio)
+
+        driver.quit()
+    }
+}
